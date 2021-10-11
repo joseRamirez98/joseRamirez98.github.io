c8e2be287d7096bb1cc0e91bec490aed1f0d210c
joseRamirez98 <34168773+joseRamirez98@users.noreply.github.com>
2021-10-10 23:11:21 -0700

    Update fancifymytext.js

--- week4/fancifymytext.js
+++ week4/fancifymytext.js
@@ -1,4 +1,4 @@
-function showAlert() {
+function changeFontSize() {
     document.getElementById("textBox").style.fontSize = "24pt";
 }
 
@@ -11,15 +11,24 @@ function makeTextUpper() {
     document.getElementById("textBox").value = str;
 }
 
+function makeTextLower(textBox) {
+    str = textBox.value;
+    str = str.toLowerCase();
+    document.getElementById("textBox").value = str;
+}
+
 function checkRadioButton() {
     var textBox = document.getElementById("textBox");
     var boringBtn = document.getElementById("boringBetty");
     var boringIsChecked = boringBtn.checked;
 
     if(boringIsChecked) {
+        textBox.style.fontSize = "initial";
         textBox.style.fontWeight = "normal";
         textBox.style.color = "black";
         textBox.style.textDecoration = "none";
+
+        makeTextLower(textBox);
     }
     else {
         textBox.style.fontWeight = "bold";
